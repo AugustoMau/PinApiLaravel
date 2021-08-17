b19e7cf6699d0e6371aaccd52fd32412d2192d13
esbasilio <e.basilio360@gmail.com>
2021-08-16 21:53:24 -0300

    nex steps

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -13,4 +14,46 @@ class PostController extends Controller
         //return view('post-list', ['posts' => $posts] );
 
     }
+
+    public function showForm()
+    {
+        $categories = Category::all();
+        return view('new-post', ['category'=>$categories]);
+    }
+
+    public function showFormEdit(Request $request)
+    {
+        $categories = Category::all();
+        $post = Post::find($request->id);
+        return view('edit-post', ['post'=>$post, 'category'=>$categories]);
+    }
+
+    public function save(Request $request)
+    {
+
+        Post::create([
+            'category_id' => $request->category,
+            'title' => $request->title,
+            'summary'=>$request->summary,
+            'image' => '',
+            'description' => $request->description,
+            'author'=> $request->author
+        ]);
+    }
+
+    public function savePostEdit(Request $request)
+    {
+        if(isset($request->id)){ // es un post ya existente
+
+            $post = Post::find($request->id);
+            $post->category_id = $request->category;
+            $post->title = $request->title;
+            $post->summary = $request->summary;
+            $post->image = '';
+            $post->description = $request->description;
+            $post->author = $request->author;
+            $post->save();
+        }
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,15 @@ Route::get('/', function () {
 });
 
 Route::get('/post-list', PostController::class . '@showList');
-//Route::get('/post-list', 'PostController@showList');
+// formulario para crear un post
+Route::get('/post-show-form', PostController::class . '@showForm');
+// metodo para guardar un post
+Route::get('/post-save', PostController::class . '@save')->name('savepost');
+
+// editar un post
+Route::get('/post-show-form-edit/{id}', PostController::class . '@showFormEdit');
+
+Route::get('/post-save-edit', PostController::class . '@savePostEdit')->name('savepost_edit');
+
+//Route::get('/post-save', PostController::class . '@save')->name('savepost');
+Route::get('/post-delete/{id}', PostController::class . '@delete');
