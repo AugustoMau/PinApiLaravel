1ed7ba5f805ff76058a61cd08c093058a7e33edb
AugustoMau <augustomauro.cba@gmail.com>
2021-11-18 14:05:44 -0300

    Envio Formulario

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 // metodo para guardar un post
-Route::post('/newuser', 'App\Http\Controllers\NewUserController@saveUsuarios');
+Route::post('/usuarionuevo', 'App\Http\Controllers\NewUserController@saveUsuarios');
 
